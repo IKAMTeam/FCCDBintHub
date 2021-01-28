0f7a11e6b41e3f7e555cee2490a90ffeeb5e465e
P4radise <emel.9616@gmail.com>
2021-01-28 12:05:09 +0700

    IHub-171841 Log does not work in the MASTER

--- get_process_id.py
+++ get_process_id.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import json
+import sys
+from json import loads
 
-def get_process_id():
-    with open('ihub_parameters.json', "rb") as PFile:
-        ihub_data = json.loads(PFile.read().decode('utf-8'))
-
+def get_process_id(PFile):
+    ihub_data = loads(PFile)
     return ihub_data['processId']
 
-if __name__ == "__main__":
-    print(get_process_id())
+print(get_process_id(sys.argv[1]))
